150f623642c7fb59b3a787e17f0fa371d34f43d6
Antônio Pedro <antoniopedro.rs9@gmail.com>
2019-05-08 19:06:09 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,98 +1,102 @@
-var w = window.innerWidth;
-var h = window.innerHeight;
-var ovule, sperm;
-
-function setup() {
-    createCanvas(w, h);
-
-    // Ovule Object:
-    ovule = createSprite(h/8, h/2, h/8, h/8);
-
-    ovule.draw = function() {
-        fill(255, 128);
-        stroke(255);
-        strokeWeight(h/64);
-
-        ellipse(0, 0, random(h/8, h/8 + 4), random(h/8, h/8 + 4));
-        ellipse(0, 0, random(h/128, h/128 + 4), random(h/128, h/128 + 4));
-    }
-
-    ovule.velocity.y = 0;
-    ovule.maxSpeed = 8;
-
-    // Sperm array:
-    sperm = new Group();
-
-    score();
+if (window.matchMedia("(orientation: portrait)").matches) {
+    alert("Rotate ur phone to play");
 }
+else {
 
-// Score functions:
-var points = 0;
+    // Global variables:
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    var ovule, sperm, cron, pontuation;
 
-function score() {
+    function setup() {
+        createCanvas(w, h);
 
-	var cron = setInterval(
-		function() {
-			points = points + 1;
+        // Ovule Object:
+        ovule = createSprite(h/8, h/2, h/8, h/8);
 
-			document.getElementById("score").innerHTML = points;
-
-			if (sperm.overlap(ovule)) {
-				points = 0;
-				clearInterval(cron);
-			}
-		} ,1000)
-}
-
-function draw() {
-    background(233, 30, 99);
-
-    // Adding spermatozoa in the sperm group and displaying it:
-    if (frameCount % 20 == 0) {
-
-        // Spermatozoom Object:
-        var spermatozoom = createSprite(w, random(h), h/16, h/32);
-        spermatozoom.velocity.x = random(-4, -8);
-        spermatozoom.life = 512;
-
-        spermatozoom.draw = function() {
-            fill(255);
-            noStroke();
-            ellipse(0, 0, random(h/16, h/16 + 4), random(h/32, h/32 + 4));
-
+        ovule.draw = function() {
             fill(255, 128);
-            ellipse(h/16, 0, random(h/8, h/8 + 4), random(h/512, h/512 + 4));
+            stroke(255);
+            strokeWeight(h/64);
+
+            ellipse(0, 0, random(h/8, h/8 + 4), random(h/8, h/8 + 4));
+            ellipse(0, 0, random(h/128, h/128 + 4), random(h/128, h/128 + 4));
         }
-        sperm.add(spermatozoom);
-    }
-
-    // Defining screen limits:
-    if (ovule.position.y >= h - h/16 - 4) {
-        ovule.position.y = h - h/16 - 4;
         ovule.velocity.y = 0;
-    }
-    else if (ovule.position.y <= 0 + h/16 + 4) {
-        ovule.position.y = 0 + h/16 + 4;
-        ovule.velocity.y = 0;
-    }
+        ovule.maxSpeed = 8;
 
-    // Adding gravity acceleration:
-    ovule.addSpeed(0.2, 90);
+        // Sperm array:
+        sperm = new Group();
 
-    // Function to move the ovule:
-    if (mouseIsPressed || keyIsPressed) {
-        ovule.addSpeed(-0.8, 90);
-    }
+        // Score functions:
+        pontuation = 0;
+        document.getElementById("scoreBoard").innerHTML = pontuation;
 
-    // Game over & Restart function:
-    if (sperm.overlap(ovule)) {
-
-    	sperm.removeSprites();
-
-    	alert("GAME OVER\nSCORE: " + points);
-
-    	location.reload();
+        cron = setInterval( function() { 
+            pontuation = pontuation + 1; 
+            document.getElementById("scoreBoard").innerHTML = pontuation;
+        }, 1000);
     }
 
-    drawSprites();
+    function draw() {
+        background(233, 30, 99);
+
+        if (frameCount % 20 == 0) {
+
+            // Spermatozoom Object:
+            var spermatozoom = createSprite(w, random(h), h/16, h/32);
+            spermatozoom.velocity.x = random(-4, -8);
+            spermatozoom.life = 512;
+
+            spermatozoom.draw = function() {
+                fill(255);
+                noStroke();
+                ellipse(0, 0, random(h/16, h/16 + 4), random(h/32, h/32 + 4));
+
+                fill(255, 128);
+                ellipse(h/16, 0, random(h/8, h/8 + 4), random(h/512, h/512 + 4));
+            }
+
+            // Adding spermatozoom to sperm group:
+            sperm.add(spermatozoom);
+        }
+
+        // Defining screen limits:
+        if (ovule.position.y >= h - h/16 - 4) {
+            ovule.position.y = h - h/16 - 4;
+            ovule.velocity.y = 0;
+        }
+        else if (ovule.position.y <= 0 + h/16 + 4) {
+            ovule.position.y = 0 + h/16 + 4;
+            ovule.velocity.y = 0;
+        }
+
+        // Adding gravity acceleration:
+        ovule.addSpeed(0.2, 90);
+
+        // Function to move the ovule:
+        if (mouseIsPressed || keyIsPressed) {
+            ovule.addSpeed(-0.8, 90);
+        }
+
+        // Game over function trigger:
+        if (sperm.overlap(ovule)) {
+            gameOver();
+        }
+
+        drawSprites();
+    }
+
+    function gameOver() {
+
+        ovule.remove();
+        sperm.removeSprites();
+
+        alert("GAME OVER\nSCORE: " + pontuation);
+
+        pontuation = 0;
+        clearInterval(cron);
+
+        setup();
+    }
 }
