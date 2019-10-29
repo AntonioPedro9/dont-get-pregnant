fe8745b8cc75517ee3449d2b0523fdffea5a0c2c
AntonioPedro9 <antoniopedro.rs9@gmail.com>
2019-10-29 02:17:08 -0300

    update

--- script.js
+++ script.js
@@ -24,139 +24,152 @@ window.addEventListener("orientationchange", () => {
 
 
 
-// show the warning if the device orientation is portrait
-if (window.orientation == 0 || window.orientation == 180) { warning.style.display = "block" }
+// show a warning if the orientation is portrait otherwise start the game
+if (window.orientation == 0 || window.orientation == 180) { warning.style.display = "block" } else {
 
 
 
-function createOvule() {
+    function createOvule() {
 
-    // the diameter is based on the screen height
-    diameter = height/8;
+        // the diameter is based on the screen height
+        diameter = height/8;
 
-    ovule = createSprite(height/8, height/2, diameter, diameter);
+        ovule = createSprite(height/8, height/2, diameter, diameter);
 
-    ovule.draw = () => {
-        fill(255, 128);
-        stroke(255);
-        strokeWeight(diameter/8);
+        ovule.draw = () => {
+            fill(255, 128);
+            stroke(255);
+            strokeWeight(diameter/8);
 
-        ellipse(0, 0, random(diameter, diameter + 4));
-        ellipse(0, 0, random(diameter/8, diameter/8 + 4));
+            ellipse(0, 0, random(diameter, diameter + 4));
+            ellipse(0, 0, random(diameter/8, diameter/8 + 4));
+        }
+
+        // set a circular collision zone
+        ovule.setCollider("circle", 0, 0, diameter/2);
     }
 
-    // set a circular collision zone
-    ovule.setCollider("circle", 0, 0, diameter/2);
-
-    ovule.maxSpeed = 8;
-}
 
 
+    function createSpermatozoom() {
 
-function createSpermatozoom() {
+        let diameterX = height/16;
+        let diameterY = height/32;
 
-    let diameterX = height/16;
-    let diameterY = height/32;
+        spermatozoom = createSprite(width, random(height) /* places the spermatozoom on a rondom place at screen */, diameterX, diameterY);
 
-    spermatozoom = createSprite(width, random(height) /* places the spermatozoom on a rondom place at screen */, diameterX, diameterY);
+        spermatozoom.draw = () => {
 
-    spermatozoom.draw = () => {
+            // spermatozoom body
+            fill(255);
+            noStroke();
+            ellipse(0, 0, random(diameterX, diameterX + 4), random(diameterY, diameterY + 4));
 
-        // spermatozoom body
-        fill(255);
-        noStroke();
-        ellipse(0, 0, random(diameterX, diameterX + 4), random(diameterY, diameterY + 4));
+            // spermatozoom tail
+            fill(255, 128);
+            ellipse(height/16, 0, random(diameterX * 2, diameterX * 2 + 4), random(diameterY/16, diameterY/16 + 4));
+        }
 
-        // spermatozoom tail
-        fill(255, 128);
-        ellipse(height/16, 0, random(diameterX * 2, diameterX * 2 + 4), random(diameterY/16, diameterY/16 + 4));
+        // spermatozoom speed is based on the screen width
+        let speed = random(-width/256, -width/128);
+        spermatozoom.velocity.x = speed;
+
+        // spermatozoom desapears after some time
+        spermatozoom.life = 512;
+
+        // add spermatozoom to sperm array
+        sperm.add(spermatozoom);
     }
 
-    // spermatozoom speed is based on the screen width
-    let speed = random(-width/256, -width/128);
-    spermatozoom.velocity.x = speed;
-
-    // spermatozoom desapears after some time
-    spermatozoom.life = 512;
-
-    // add spermatozoom to sperm array
-    sperm.add(spermatozoom);
-}
 
 
-
-function score() {
-    pontuation = 0;
-    scoreBoard.innerHTML = pontuation;
-
-    // pontuation is based on how many seconds you survive
-    chronometer = setInterval( () => { 
-        pontuation += 1; 
+    function score() {
+        pontuation = 0;
         scoreBoard.innerHTML = pontuation;
-    }, 1000);
-}
 
-
-
-function gameRules() {
-
-    // define screen limits
-    if (ovule.position.y > height - diameter/2 - 4) {
-        ovule.position.y = height - diameter/2 - 4;
-        ovule.velocity.y = 0;
-    }
-    else if (ovule.position.y < 0 + diameter/2 + 4) {
-        ovule.position.y = 0 + diameter/2 + 4;
-        ovule.velocity.y = 0;
+        // pontuation is based on how many seconds you survive
+        chronometer = setInterval( () => { 
+            pontuation += 1; 
+            scoreBoard.innerHTML = pontuation;
+        }, 1000);
     }
 
-    if (keyIsPressed || mouseIsPressed) {
+
+
+    function gameRules() {
+
+        // define screen limits
+        if (ovule.position.y > height - diameter/2 - 4) {
+            ovule.position.y = height - diameter/2 - 4;
+            ovule.velocity.y = 0;
+        }
+        else if (ovule.position.y < 0 + diameter/2 + 4) {
+            ovule.position.y = 0 + diameter/2 + 4;
+            ovule.velocity.y = 0;
+        }
+
+        let speed = height/(height/4);
+
+        if (keyIsDown(UP_ARROW)) {
+            ovule.velocity.y = -speed;
+        }
+        else if (keyIsDown(DOWN_ARROW)) {
+            ovule.velocity.y = speed;
+        }
+
+        if (mouseIsPressed) {
+            if (mouseY >= height/2) {
+                ovule.velocity.y = speed;
+            }
+            else {
+                ovule.velocity.y = -speed;
+            }
+        }
+
+        // call gameover if the spermatozoom collide with ovule
+        sperm.overlap(ovule, gameOver);
+    }
+
+
+
+    function gameOver() {
         
+        // remove all sprites
+        ovule.remove();
+        sperm.removeSprites();
+
+        // reset the score
+        pontuation = 0;
+        clearInterval(chronometer);
+
+        // restart the game
+        setup();
     }
 
-    // call gameover if the spermatozoom collide with ovule
-    sperm.overlap(ovule, gameOver);
-}
 
 
+    function setup() {
+        canvas = createCanvas(width, height);
+        canvas.parent("container");
 
-function gameOver() {
-    
-    // remove all sprites
-    ovule.remove();
-    sperm.removeSprites();
+        // create a spermatozoom array
+        sperm = new Group();
 
-    // reset the score
-    pontuation = 0;
-    clearInterval(chronometer);
-
-    // restart the game
-    setup();
-}
-
-
-
-function setup() {
-    canvas = createCanvas(width, height);
-    canvas.parent("container");
-
-    // create a spermatozoom array
-    sperm = new Group();
-
-    createOvule();
-    score();
-}
-
-
-
-function draw() {
-    background(233, 30, 99);
-    
-    gameRules();
-
-    // create a new spermatozoom based on the frame cout
-    if (frameCount % 10 == 0) {
-        createSpermatozoom();
+        createOvule();
+        score();
     }
-    drawSprites();
+
+
+
+    function draw() {
+        background(233, 30, 99);
+        
+        gameRules();
+
+        // create a new spermatozoom based on the frame cout
+        if (frameCount % 10 == 0) {
+            createSpermatozoom();
+        }
+        drawSprites();
+    }
 }
